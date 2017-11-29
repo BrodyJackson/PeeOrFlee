d169626ac9a26aa0af7b56eb39f098edae71f5ef
BrodyJackson <brodyjackson1@gmail.com>
2017-11-28 21:45:34 -0700

    added items to front-page, not responsive

--- front_end/src/components/Header.js
+++ front_end/src/components/Header.js
@@ -3,12 +3,22 @@ import '../App.css';
 import Searchbar from './Searchbar.js';
 
 class Header extends Component {
+
+    buttonClick(){
+
+    }
     render(){
         return (
             <div> 
                 <div className="header">
                     <Searchbar></Searchbar>
-                    <img className = "header_image" src = {require("../images/header.jpg")} alt=""></img>   
+                    <div className="button_flex_container">
+                        <button type="button" className = "newFacilityButton" onClick={this.buttonClick}> Add New Facility </button>
+                        <button type="button" className = "viewBestButton" onClick={this.buttonClick}> View Best Facility's </button> 
+                        <button type="button" className = "viewRecentButton" onClick={this.buttonClick}> View Recent Ratings </button> 
+                    </div>
+                    <img className = "header_image" src = {require("../images/header.jpg")} alt=""></img> 
+                    <img className = "uni_logo" src = {require("../images/uc_logo.png")} alt=""></img>  
                 </div>
                 
             </div> 

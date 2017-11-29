7b81e97de09aab069060f6c7c12c5cdc8bfca6f5
BrodyJackson <brodyjackson1@gmail.com>
2017-11-28 22:34:45 -0700

    started attempting to setup the search

--- front_end/src/components/Searchbar.js
+++ front_end/src/components/Searchbar.js
@@ -3,14 +3,44 @@ import '../App.css';
 
 
 class Searchbar extends Component {
+    constructor(props){
+        super(props); 
+        this.state = {
+            allBathrooms: [], 
+            inCurrentSearch: [], 
+            searchValue : ""
+        }; 
+    }
+
+    componentDidMount(){
+        console.log("hello"); 
+        fetch("/bathrooms")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data); 
+            this.setState({ allBathrooms : data });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+    
+    handleChange(e){
+        e.preventDefault(); 
+        console.log(e.target.value); 
+        this.setState( { searchValue : e.target.value})
+    }
+
+
     render(){
         return (
             <div className="searchBox">
                 <input
+                value = {this.state.searchValue}
                 className="search_field" 
                 type="text"
                 placeholder="Enter a Building..."
-                onChange = {this.handleChange}
+                onChange = {this.handleChange.bind(this)}
                 />
             </div>
         );

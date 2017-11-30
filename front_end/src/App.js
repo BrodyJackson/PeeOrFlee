3549861fbdc89732b31cb14a4690a3c6e45c86ec
BrodyJackson <brodyjackson1@gmail.com>
2017-11-30 12:00:28 -0700

    result menu working, ability to close working, need to add rating values to popup

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.css'; 
 import Header from './components/Header.js'; 
 
 class App extends Component {
--- front_end/src/components/Header.js
+++ front_end/src/components/Header.js
@@ -6,9 +6,12 @@ class Header extends Component {
     constructor(props){
         super(props); 
         this.state = { 
-            selectedId : -1 
+            selectedId : -1, 
+            currentWashroomView: null, 
+            currentRatings : []
         }
         this.searchResultsClick = this.searchResultsClick.bind(this); 
+        this.closeWashMenu = this.closeWashMenu.bind(this); 
     }
 
     buttonClick(){
@@ -16,10 +19,82 @@ class Header extends Component {
     }
 
     searchResultsClick(washroom) {
-        alert("clicked"); 
-        console.log(washroom); 
-        // console.log(e.target); 
+        
+        this.setState({selectedId : washroom}); //changed the selected id to the one we want 
+        let fetchBathroomString = ("/bathrooms/" + washroom); 
+        let fetchRatingString = ("/ratings/bathroomrating/" + washroom);
+        //if we have selected a washroom, then get back the washroom with that id from the database
+        if(this.state.selectedId !== -1){
+            fetch(fetchBathroomString)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data, "retrieved"); 
+                this.setState({ currentWashroomView : data });
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+            //fetch all the ratings for that specific washroom
+            fetch(fetchRatingString)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data, "rating retrieved"); 
+                this.setState({ currentRatings : data });
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+
+            //add function to calculate the averages for the ratings, then these are added to the ratings
+        }
     }
+
+    closeWashMenu(){
+        this.setState({currentWashroomView : null, currentRatings : []}); 
+    }
+
+    showWashroom() {
+        if(this.state.currentWashroomView !== null){
+            let current = this.state.currentWashroomView[0];
+            let title = (current.building + " " + current.room_num);  
+            return( 
+                <div className = "washroomInfoContainer" onClick = {this.closeWashMenu}>
+                    <div className = "infoRow close">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className = "infoRow">
+                        <h1 className = "title" >{title}</h1> 
+                    </div>
+                    <div className = "infoRow">
+                        <div className = "category">
+                            <p className = "subTitle">Location</p> 
+                            {/* add value */}
+                        </div>
+                        <div className = "category">
+                            <p className = "subTitle">Smell</p> 
+                        </div> 
+                        <div className = "category">
+                            <p className = "subTitle">Cleanliness</p> 
+                        </div>  
+                    </div>  
+                    <div className = "infoRow">
+                        <div className = "category">
+                            <p className = "subTitle">Maintanence</p> 
+                        </div>
+                        <div className = "category">
+                            <p className = "subTitle">Wait time</p> 
+                        </div> 
+                        <div className = "category">
+                            <p className = "subTitle">Privacy</p> 
+                        </div> 
+                    </div>  
+
+                </div>
+            )
+        }
+    }
+    
+
     render(){
         return (
             <div> 
@@ -33,6 +108,7 @@ class Header extends Component {
                     <img className = "header_image" src = {require("../images/header.jpg")} alt=""></img> 
                     <img className = "uni_logo" src = {require("../images/uc_logo.png")} alt=""></img>  
                 </div>
+                {this.showWashroom()}
                 
             </div> 
         );
